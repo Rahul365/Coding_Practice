94604e320a02ff942a04e359e2790f0c46e02b83
Rahul Dhiman <rahul.dhiman365@gmail.com>
2022-01-13 23:05:51 +0530

    Fix kthPermutation code

--- MathQ/kthPermutation.cpp
+++ MathQ/kthPermutation.cpp
@@ -8,36 +8,33 @@ class Solution
 public:
     string getPermutation(int n, int k)
     {
-        --k;         // making it 0-based
-        int fact[n]; // factorials from 0 to n-1
-        fact[0] = 1;
+        --k;          // making it 0-based
+        int fact = 1; // factorials from 0 to n-1
         for (int i = 1; i < n; ++i)
         {
-            fact[i] = fact[i - 1] * n - 1;
+            fact = fact * i;
         }
         // prepare the first permutation of length n
-        string s(n,'1');
-        for (int i = 1; i < n; ++i)
-        {
-            s[i] = s[i - 1] + 1;
-        }
+        vector<int> s(n);
+        iota(s.begin(), s.end(), 1);
         string ans = "";
         for (int i = n - 1; i >= 0; --i)
         {
-            int index = k / fact[i];    // bucket in which kth permutation sequence falls in
-            ans += s[index];            // append the character at position index to the answer
-            s.erase(s.begin() + index); // remove the character at 'index' from s
-            k = k % fact[i];            // index of the next character
+            int index = k / fact;            // bucket in which kth permutation sequence falls in
+            ans.append(to_string(s[index])); // append the character at position index to the answer
+            s.erase(s.begin() + index);      // remove the character at 'index' from s
+            k = k % fact;
+            fact /= max(i, 1); // index of the next character
         }
         return ans;
     }
-}
+};
 
-int
-main()
+int main()
 {
     int n, k;
     cin >> n >> k;
-
+    Solution s;
+    cout << s.getPermutation(n, k) << "\n";
     return 0;
 }
